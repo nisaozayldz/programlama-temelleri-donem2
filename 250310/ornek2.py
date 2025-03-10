1b70c048e5c90b1bb8273f253f62f236bbdb2528
nisaozayldz <nisahnk4@gmail.com>
2025-03-10 12:07:35 +0300

    Add files via upload

---
+++ 250310/ornek2.py
@@ -0,0 +1,6 @@
+k_ad=input("kullanıcı adını gir")
+sifre=input("şifreyi gir")
+if(k_ad=="Türkiye" and sifre=="1923"):
+    print("Giriş başarılı")
+else:
+    print("Giriş başarısız")
---
+++ 250310/ornek3.py
@@ -0,0 +1,5 @@
+s=int(input("sayıyı giriniz: "))
+if s%3==0 and s%5==0:
+    print("sayı 15'e tam bölünür")
+else:
+    print("sayı 15'e tam bölünmez")
